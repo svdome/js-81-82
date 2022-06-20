b73cf3b58a3484e6a070ca73c21ff8e962416c3a
svdome <svdome@yandex.ru>
2022-06-20 17:08:53 +0300

    task lesson 81-82

---
+++ js/task.js
@@ -0,0 +1,23 @@
+/**
+ * @param {object[]} users
+ * @param {number} users.id
+ * @param {string} users.name
+ * @param {object} [users.subscription]
+ * @param {object} [users.subscription.info]
+ * @param {number} [users.subscription.info.value]
+ */
+ const getTotalSales = users => {
+	let total = 0;
+    users.forEach(user => {
+        total = total + (user.subscription?.info?.value ?? 0);
+    })
+    return total;
+}
+
+// Sample usage - do not modify
+const users = [
+  {id: 1, name: "Alex"},
+  {id: 2, name: "Sam", subscription: {info: {value: 59}}},
+  {id: 3, name: "Charlie", subscription: {info: {value: 31}}}
+];
+console.log(getTotalSales(users)); // 90
